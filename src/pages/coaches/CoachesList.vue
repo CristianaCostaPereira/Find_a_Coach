d01e14d31cb4a474ebd7897e4ca54e7458f6bacd
cristiana.costa.ext <cristiana.costa.ext@spms.min-saude.pt>
2022-08-09 16:01:10 +0100

    TheHeader component created an its layout; App file styling and TheHeader import; Start working on CoachesList layout.

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    FILTER
+  </section>
+
+  <section>
+    List of coaches
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      choaches: []
+    }
+  }
+}
+</script>
