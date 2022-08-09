8889745bab738d91ad1109a6e3c0b3c38d5569ad
cristiana.costa.ext <cristiana.costa.ext@spms.min-saude.pt>
2022-08-09 20:24:50 +0100

    CoachDetail component layout and script.

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,13 +1,76 @@
 <template>
-  Details for Coach
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
 
-  <router-view></router-view>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
 
-  <router-link :to="{ name: 'coach-contact', params: { id: id } }">Contact</router-link>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+
+        <base-button
+          link
+          :to="{ name: 'coach-contact', params: { id: id } }">
+
+          Contact
+        </base-button>
+      </header>
+    </base-card>
+
+    <router-view></router-view>
+  </section>
+
+  <section>
+    <base-card>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+
+        :type="area"
+        :title="area">
+      </base-badge>
+
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
 
 <script>
 export default {
-  props: ['id']
+  props: ['id'],
+
+  data() {
+    return {
+      selectedCoach: null
+    }
+  },
+
+  computed: {
+    fullName () {
+      return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName
+    },
+
+    areas () {
+      return this.selectedCoach.areas
+    },
+
+    rate () {
+      return this.selectedCoach.hourlyRate
+    },
+
+    description () {
+      return this.selectedCoach.description
+    }
+  },
+
+  created () {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+      coach => coach.id === this.id
+    )
+  }
 }
 </script>
