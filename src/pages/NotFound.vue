3dbdc8518a66083c6201c9e7a7cd7637d902fd0f
cristiana.costa.ext <cristiana.costa.ext@spms.min-saude.pt>
2022-08-12 15:48:49 +0100

    Not found component layout.

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found ❌</h2>
+
+      <p>This page could not be found.</p>
+
+      <p>Maybe check all of our
+        <router-link :to="{ name: 'coaches-list' }">
+          coaches
+        </router-link>.
+      </p>
+    </base-card>
+  </section>
+</template>
